2fa23f8131c547e399aa6ccbebc7d06ca90c2da2
Jayanth Enugala <864682+jayanthenugala@users.noreply.github.com>
2019-01-03 19:14:25 -0500

    Add files via upload

---
+++ weather.sql
@@ -0,0 +1,15 @@
+SELECT * 
+FROM city_data
+WHERE (country = 'Iran' AND city = 'Tabriz' 
+       AND year BETWEEN 1808 AND 2013) 
+   OR (country = 'India' AND city = 'Hyderabad'
+       AND year BETWEEN 1808 AND 2013)
+   OR (country = 'Russia' AND city = 'Moscow'
+       AND year BETWEEN 1808 AND 2013);
+  
+
+
+SELECT *
+FROM city_list
+WHERE country IN ('India', 'Iran', 'Russia')
+ORDER BY country;
